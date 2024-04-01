93cb3dec74f458d6846fa920c278621c21bc0a92
Rei1449 <rei0409@toreiwatarunonotobukkukonpyuta.local>
2024-04-01 15:49:50 +0900

    エラーの該当だと考えられる箇所を修正

--- register_user/register_user.py
+++ register_user/register_user.py
@@ -18,21 +18,22 @@ dynamodb = session.resource("dynamodb")
 
 def register_user(event, context):
   user = json.loads(event)
+  user_name = user["name"]
   register_table = dynamodb.Table("janken-users")
   confilm = register_table.scan(
-    FilterExpression = Attr("name").eq(f"{user["name"]}")
+    FilterExpression = Attr("name").eq(f"{user_name}")
   )
   if confilm["Count"] == 0:
     register_pass_hash = hashlib.sha256()
     register_pass_hash.update(user["password"].encode())
     response = register_table.put_item(
       Item={
-        "name": user["name"],
+        "name": user_name,
         "password": register_pass_hash.hexdigest(),
         "markov_id": 0,
         "game_count": 0,
         "previous_id": 0,
       }
     )
-    return json.dumps({"name":user["name"], "markov_id":0, "game_count":0, "previous_id":0})
+    return json.dumps({"name":user_name, "markov_id":0, "game_count":0, "previous_id":0})
   return json.dumps({"error":"そのユーザーはすでに登録されています。別のユーザー名で登録してください。"})
